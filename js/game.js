13d74c24f0c19841b119668ee469f18d6f22cedb
Gyula Szvitek <gy.szvitek@gmail.com>
2020-01-21 23:53:53 +0000

    feat: assets added, displaying the player and some dragons

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ gameScene.preload = function() {
   // load images
   this.load.image("background", "assets/background.png");
   this.load.image("player", "assets/player.png");
+  this.load.image("enemy", "assets/dragon.png");
 };
 
 gameScene.create = function() {
@@ -15,6 +16,14 @@ gameScene.create = function() {
     this.sys.game.config.width / 2,
     this.sys.game.config.height / 2
   );
+
+  this.add.sprite(50, 180, "player");
+
+  let enemy1 = this.add.sprite(250, 180, "enemy");
+  enemy1.flipX = true;
+  enemy1.setScale(0.75);
+  let enemy2 = this.add.sprite(450, 180, "enemy");
+  enemy2.flipX = true;
 };
 
 // set the configuration of the game
