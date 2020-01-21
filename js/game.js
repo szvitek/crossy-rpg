e5e67a7739b8014888ff3c47d344fcf5bbcd2433
Gyula Szvitek <gy.szvitek@gmail.com>
2020-01-21 23:44:01 +0000

    feat: player and background preloaded, bg created

--- js/game.js
+++ js/game.js
@@ -1,6 +1,22 @@
 // create a new scene
 let gameScene = new Phaser.Scene("Game");
 
+// load assets
+gameScene.preload = function() {
+  // load images
+  this.load.image("background", "assets/background.png");
+  this.load.image("player", "assets/player.png");
+};
+
+gameScene.create = function() {
+  // create bg sprite
+  let bg = this.add.sprite(0, 0, "background");
+  bg.setPosition(
+    this.sys.game.config.width / 2,
+    this.sys.game.config.height / 2
+  );
+};
+
 // set the configuration of the game
 let config = {
   type: Phaser.AUTO,
