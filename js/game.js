8807d2d1a974e6c625542e6ad0bafe3190dc8819
Gyula Szvitek <gy.szvitek@gmail.com>
2020-01-23 22:54:09 +0000

    feat: detecting user input, moving the player, init method, goal + sprite overlap check

--- js/game.js
+++ js/game.js
@@ -1,12 +1,18 @@
 // create a new scene
 let gameScene = new Phaser.Scene("Game");
 
+// initiate scene parameters
+gameScene.init = function() {
+  this.playerSpeed = 3;
+};
+
 // load assets
 gameScene.preload = function() {
   // load images
   this.load.image("background", "assets/background.png");
   this.load.image("player", "assets/player.png");
   this.load.image("enemy", "assets/dragon.png");
+  this.load.image("goal", "assets/treasure.png");
 };
 
 gameScene.create = function() {
@@ -21,10 +27,34 @@ gameScene.create = function() {
 
   // reduce the scale of the player by 50%
   this.player.setScale(0.5);
+
+  // goal
+  this.goal = this.add
+    .sprite(
+      this.sys.game.config.width - 80,
+      this.sys.game.config.height / 2,
+      "goal"
+    )
+    .setScale(0.5);
 };
 
 // this is called up to 60 times per sec
-gameScene.update = function() {};
+gameScene.update = function() {
+  // check for active input
+  if (this.input.activePointer.isDown) {
+    this.player.x += this.playerSpeed;
+  }
+
+  // treasure overlap check
+  let playerRect = this.player.getBounds();
+  let treasureRect = this.goal.getBounds();
+
+  if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, treasureRect)) {
+    // restart the scene
+    this.scene.restart();
+    return;
+  }
+};
 
 // set the configuration of the game
 let config = {
