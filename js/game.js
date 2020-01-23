6af2f738ed7da31c4413d10d3fd694f13f5125b7
Gyula Szvitek <gy.szvitek@gmail.com>
2020-01-23 22:29:19 +0000

    chore: clean up the playground code

--- js/game.js
+++ js/game.js
@@ -12,37 +12,19 @@ gameScene.preload = function() {
 gameScene.create = function() {
   // create bg sprite
   let bg = this.add.sprite(0, 0, "background");
-  bg.setPosition(
-    this.sys.game.config.width / 2,
-    this.sys.game.config.height / 2
-  );
 
+  // change the origin to the top-left corner
+  bg.setOrigin(0, 0);
+
+  // create the player
   this.player = this.add.sprite(50, 180, "player");
 
-  this.enemy1 = this.add.sprite(250, 180, "enemy");
-  this.enemy1.flipX = true;
-  this.enemy1.setScale(0.75);
-  this.enemy2 = this.add.sprite(450, 180, "enemy");
-  this.enemy2.flipX = true;
-
-  // rotate by.angle
-  // this.player.setAngle(-45);
-  this.player.angle = -45;
-
-  // rotate by rad
-  // this.enemy2.setRotation(Math.PI / 4);
-  this.enemy2.rotation = Math.PI / 4;
+  // reduce the scale of the player by 50%
+  this.player.setScale(0.5);
 };
 
 // this is called up to 60 times per sec
-gameScene.update = function() {
-  // this.enemy1.x += 1;
-  // this.enemy1.angle++;
-  if (this.enemy1.scaleX < 2) {
-    this.enemy1.scaleX += 0.01;
-    this.enemy1.scaleY += 0.01;
-  }
-};
+gameScene.update = function() {};
 
 // set the configuration of the game
 let config = {
